6147c62eea6dd778538dc270050a4e927b444a32
Vincent Meurisse <dev@meurisse.org>
2014-05-15 09:04:18 +0200

    Use commander for options parsing

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,11 @@
+var commander = require('commander');
 
 module.exports = function() {
-	require('./wlint').lint({folder: '.'}, function(err, lint) {
+	commander.version(require('../package.json').version)
+	         .usage('[options] [folder] — Lint files found in the given folder. Defaults to the current folder')
+	         .parse(process.argv);
+	
+	require('./wlint').lint({folder: commander.args[0] || '.'}, function(err, lint) {
 		var status = err ? 1 : lint.length ? 2 : 0;
 		require('exit')(status);
 	});
